9d5e9d3b41ef612b2bc0f97041a12d438a6939fe
adailsonaguiar <adailsonacj@live.com>
2024-07-05 23:49:34 -0300

    kafka producer and consumer apps

--- src/fraud_detector/index.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FraudDetectorService } from './index.service';
-
-@Module({
-  providers: [FraudDetectorService],
-  exports: [FraudDetectorService],
-})
-export class FraudDetectorModule {}
--- src/new_order/index.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrderService } from './index.service';
-
-@Module({
-  providers: [OrderService],
-  exports: [OrderService],
-})
-export class KafkaModule {}
